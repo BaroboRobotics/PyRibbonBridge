d4cc917cb6f5b286c6d2b60349bf3a2f6ec7db32
David Ko <david.dk.ko@gmail.com>
2016-02-10 13:31:48 -0800

    Changed version number to 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 setup (name = 'PyRibbonBridge',
        author = 'David Ko',
        author_email = 'david@barobo.com',
-       version = '0.1',
+       version = '0.0.1',
        description = "This is a pure Python implementation of ribbon-bridge: An"
        "RPC Framework http://github.com/BaroboRobotics/ribbon-bridge",
        package_dir = {'':'src'},
