92d3858f18932c38e5cc983e2f8cc91ece57989e
David Ko <david.dk.ko@gmail.com>
2016-02-18 11:32:55 -0800

    Fixed missing space in description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup (name = 'PyRibbonBridge',
        author = 'David Ko',
        author_email = 'david@barobo.com',
        version = '0.0.2',
-       description = "This is a pure Python implementation of ribbon-bridge: An"
+       description = "This is a pure Python implementation of ribbon-bridge: An "
        "RPC Framework http://github.com/BaroboRobotics/ribbon-bridge",
        package_dir = {'':'src'},
        packages = ['ribbonbridge'],
