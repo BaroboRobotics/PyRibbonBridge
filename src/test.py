1c37d1923bbafed3f559b3c2a2e07cbf1a1d384a
David Ko <david@barobo.com>
2016-05-11 17:33:30 -0700

    Updated test demo

--- src/test.py
+++ src/test.py
@@ -1,34 +1,11 @@
 #!/usr/bin/env python3
 
 import ribbonbridge as rb
-import sfp.asyncio
+#import sfp.asyncio
 import asyncio
-import threading
 import logging
+import websockets
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-
-class IoCore():
-    def __init__(self):
-        self._initializing = True
-        self._initializing_sig = threading.Condition()
-        self.loop = None
-        self._thread = threading.Thread(target=self.work)
-        self._thread.start()
-
-        self._initializing_sig.acquire()
-        while self._initializing:
-            logging.info('Wake: Sig is {}'.format(self._initializing))
-            self._initializing_sig.wait(timeout=1)
-        self._initializing_sig.release()
-
-    def work(self):
-        self.loop = asyncio.new_event_loop()
-        self._initializing_sig.acquire()
-        self._initializing = False
-        self._initializing_sig.notify_all()
-        self._initializing_sig.release()
-        logging.info('Starting event loop.')
-        self.loop.run_forever()
 
 class SfpProxy(rb.Proxy):
     def __init__(self, *args, **kwargs):
@@ -37,30 +14,49 @@ class SfpProxy(rb.Proxy):
     def set_protocol(self, protocol):
         self._protocol = protocol
 
-    async def rb_emit_to_server(self, bytestring):
-        self._protocol.write(bytestring)
+    @asyncio.coroutine
+    def rb_emit_to_server(self, bytestring):
+        print('Sending bytestring: ', bytestring)
+        yield from self._protocol.send(bytestring)
 
-def main():
-    io = IoCore()
-    
-    coro = io.loop.create_connection(sfp.asyncio.SfpProtocol, 
-            'localhost', '42000')
-    fut = asyncio.run_coroutine_threadsafe(coro, io.loop)
-    (transport, protocol) = fut.result()
+@asyncio.coroutine
+def consumer(source, dst):
+    print('Consumer started.')
+    while True:
+        msg = yield from source.recv()
+        print('Received message. Delivering...')
+        yield from dst.rb_deliver(msg)
+
+@asyncio.coroutine
+def task():
+    #coro = io.loop.create_connection(sfp.asyncio.SfpProtocol, 
+    #        'localhost', '42000')
+
+    protocol = yield from websockets.connect('ws://localhost:42000')
+    #fut = asyncio.run_coroutine_threadsafe(coro, io.loop)
+    #(transport, protocol) = fut.result()
 
     daemon_proxy = SfpProxy(
-            '/home/dko/Projects/Barobo/PyLinkbot/rpc/daemon_pb2.py',
-            io.loop)
+            '/home/dko/Projects/Barobo/PyLinkbot3/src/linkbot3/async/daemon_pb2.py')
     daemon_proxy.set_protocol(protocol)
     #daemon_proxy.rb_emit_to_server = protocol.write
-    protocol.deliver = daemon_proxy.rb_deliver
+    #protocol.deliver = daemon_proxy.rb_deliver
+    asyncio.ensure_future(consumer(protocol, daemon_proxy))
 
-    daemon_proxy.rb_connect()
+    yield from asyncio.sleep(2)
+
+    print('Connecting to daemon...')
+    yield from daemon_proxy.rb_connect()
 
     args = daemon_proxy.rb_get_args_obj('sendRobotPing')
     destination = args.destinations.add()
     destination.value = 'DGKR'
-    print("PING: ", daemon_proxy.sendRobotPing(args).result())
+    print('Sending robot ping...')
+    fut = yield from daemon_proxy.sendRobotPing(args)
+    print(fut)
+    print('waiting for ping result...')
+    result = yield from fut
+    print("PING: ", result)
 
     args = daemon_proxy.rb_get_args_obj('resolveSerialId')
     print(args)
@@ -73,5 +69,8 @@ def main():
     '''
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    loop.run_until_complete(task())
+    loop.close()
 #proxy = rb.Proxy('/home/dko/Projects/Barobo/PyLinkbot/rpc/robot_pb2.py')
